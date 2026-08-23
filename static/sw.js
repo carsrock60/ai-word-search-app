54764f9281d429472d33943e45a3425720a06799
carsrock60 <aarubiam1307@gmail.com>
2026-08-23 04:31:00 +0000

    Add PWA support

---
+++ static/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'wordgen-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/style.css',
+  '/static/script.js',
+  '/static/favicon.svg',
+  '/static/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Serve cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
